ca41b793cbdfccdf73e219f24918f910ddfb98ad
kristof de spiegeleer <kristof@greenitglobe.com>
2017-01-03 22:22:27 +0400

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -6,60 +6,63 @@ cd $HOME/nim
 
 
 function nim_compile {
-    cd $HOME/nim
-    if [ -d "$HOME/nim/Nim" ]; then
-        cd Nim
-        git pull
-        git checkout tags/v0.15.2
+    if [ -e $TMPDIR/nimscale_compile_done ] ; then
+        echo "NO NEED TO COMPILE"
     else
-        git clone https://github.com/nim-lang/Nim.git
-        cd Nim
-        #git checkout -b master origin/master
-        git checkout tags/v0.15.2            
+        cd $HOME/nim
+        if [ -d "$HOME/nim/Nim" ]; then
+            cd Nim
+            git pull
+            git checkout tags/v0.15.2
+        else
+            git clone https://github.com/nim-lang/Nim.git
+            cd Nim
+            #git checkout -b master origin/master
+            git checkout tags/v0.15.2            
+        fi
+        if [ -d "$HOME/nim/Nim/csources" ]; then
+            cd csources
+            git pull
+            git checkout tags/v0.15.2             
+        else
+            git clone https://github.com/nim-lang/csources
+            cd csources
+            #git checkout -b master origin/master
+            git checkout tags/v0.15.2     
+        fi
+
+        sh build.sh
+        cd ..
+        bin/nim c koch
+        ./koch boot -d:release
+        #./koch tools
+        sh build_tools.sh
+        ./koch web
+        #./koch nimble
+
+        rm -f /usr/bin/nim*
+        rm -f /usr/local/bin/nim*
+        rm -rf /usr/local/lib/nim*
+        rm -rf /usr/lib/nim*
+        rm -f /usr/bin/koch*
+        rm -f /usr/local/bin/koch*
+
+        ln -s ~/nim/Nim/lib/ /usr/local/lib/nim
+        ln -s ~/nim/Nim/bin/nim /usr/local/bin/nim
+
+        nim e install_nimble.nims
+
+        ln -s ~/nim/Nim/bin/nimble /usr/local/bin/nimble
+        ln -s ~/nim/Nim/bin/nimgrep /usr/local/bin/nimgrep
+        ln -s ~/nim/Nim/bin/nimsuggest /usr/local/bin/nimsuggest
+        ln -s ~/nim/Nim/koch /usr/local/bin/koch
+        ln -s ~/nim/Nim/tools/nimweb /usr/local/bin/nimweb
+
+        #DONT USE KOCH, doesn't work on osx for geninstall, which creates installer
+        # ./koch geninstall
+        # sh install.sh install
+        touch $TMPDIR/nimscale_compile_done
     fi
-    if [ -d "$HOME/nim/Nim/csources" ]; then
-        cd csources
-        git pull
-        git checkout tags/v0.15.2             
-    else
-        git clone https://github.com/nim-lang/csources
-        cd csources
-        #git checkout -b master origin/master
-        git checkout tags/v0.15.2     
-    fi
-
-    sh build.sh
-    cd ..
-    bin/nim c koch
-    ./koch boot -d:release
-    #./koch tools
-    sh build_tools.sh
-    ./koch web
-    #./koch nimble
-    
-    rm -f /usr/bin/nim*
-    rm -f /usr/local/bin/nim*
-    rm -rf /usr/local/lib/nim*
-    rm -rf /usr/lib/nim*
-    rm -f /usr/bin/koch*
-    rm -f /usr/local/bin/koch*
-    
-    ln -s ~/nim/Nim/lib/ /usr/local/lib/nim
-    ln -s ~/nim/Nim/bin/nim /usr/local/bin/nim
-
-    nim e install_nimble.nims
-
-    ln -s ~/nim/Nim/bin/nimble /usr/local/bin/nimble
-    ln -s ~/nim/Nim/bin/nimgrep /usr/local/bin/nimgrep
-    ln -s ~/nim/Nim/bin/nimsuggest /usr/local/bin/nimsuggest
-    ln -s ~/nim/Nim/koch /usr/local/bin/koch
-    ln -s ~/nim/Nim/tools/nimweb /usr/local/bin/nimweb
-
-    #DONT USE KOCH, doesn't work on osx for geninstall, which creates installer
-    # ./koch geninstall
-    # sh install.sh install
-
-
 }
 
 function setenv {
@@ -70,6 +73,9 @@ function setenv {
 }
 
 function install_deps {
+    if [ -e $TMPDIR/nimscale_deps_done ] ; then
+        echo "NO NEED TO INSTALL DEPS"
+    else
     cd $HOME/nim
     nimble install nimlua -y
     nimble install libsodium -y
@@ -84,7 +90,7 @@ function install_deps {
     nimble install libnotify -y
     nimble install jwt -y
     nimble install lmdb -y
-    #nimble install msgpack4nim   -y
+    
     nimble install nimlz4 -y
     nimble install nimongo -y
     nimble install nimPDF -y
@@ -92,36 +98,38 @@ function install_deps {
     nimble install nimyaml -y
     nimble install notifications  -y
     nimble install oauth -y
-    nimble install rbtree -y
-    nimble install reactor -y
-    nimble install RingBuffer  -y
-    nimble install shorturl  -y
-    nimble install signals -y
-    nimble install sophia -y
-    nimble install sphinx  -y
-    nimble install struct -y
-    nimble install telebot -y
-    nimble install tuples  -y
-    nimble install uri2   -y
-    nimble install websocket  -y
-    nimble install yaml  -y
-    nimble install zip  -y
-    nimble install enet  -y #http://enet.bespin.org/Features.html
-    nimble install teafiles  -y #https://github.com/unicredit/nim-teafiles
-    nimble install rethinkdb  -y
-    nimble install otp  -y
-    #nimble install nimrpc -y
-    nimble install fileinput -y
-    nimble install pudge -y
-    
+        nimble install rbtree -y
+        nimble install reactor -y
+        nimble install RingBuffer  -y
+        nimble install shorturl  -y
+        nimble install signals -y
+        nimble install sophia -y
+        nimble install sphinx  -y
+        nimble install struct -y
+        nimble install telebot -y
+        nimble install tuples  -y
+        nimble install uri2   -y
+        nimble install websocket  -y
+        nimble install yaml  -y
+        nimble install zip  -y
+        nimble install enet  -y #http://enet.bespin.org/Features.html
+        nimble install teafiles  -y #https://github.com/unicredit/nim-teafiles
+        nimble install rethinkdb  -y
+        nimble install otp  -y
+        #nimble install nimrpc -y
+        nimble install fileinput -y
+        nimble install pudge -y
 
-    #NOT WORKING
-    # nimble install nimstopwatch -y
-    # nimble install orientdb  -y #http://orientdb.com/orientdb/
-    # nimble install nim-routine -y
-    # nimble install nim-fnmatch -y
-    # nimble install 'https://github.com/singularperturbation/nim-leveldb' -y
 
+        #NOT WORKING
+        # nimble install nimstopwatch -y
+        # nimble install orientdb  -y #http://orientdb.com/orientdb/
+        # nimble install nim-routine -y
+        # nimble install nim-fnmatch -y
+        # nimble install 'https://github.com/singularperturbation/nim-leveldb' -y
+        #nimble install msgpack4nim   -y
+        touch $TMPDIR/nimscale_deps_done
+    fi
 }
 
 function install_nimscale {
@@ -163,9 +171,6 @@ function playenv {
 
 ############
 
-# rm -f $TMPDIR/nimscale_compile_done
-# rm -f $TMPDIR/nimscale_deps_done
-# rm -f $TMPDIR/nimscale_done
 
 
 if [ "$(uname)" == "Darwin" ]; then
@@ -183,3 +188,10 @@ setenv
 install_deps
 install_nimscale
 playenv
+
+#at end remove the files for done, because if we call this again I am sure goal is to redo it
+rm -f $TMPDIR/nimscale_compile_done
+rm -f $TMPDIR/nimscale_deps_done
+rm -f $TMPDIR/nimscale_done
+
+
