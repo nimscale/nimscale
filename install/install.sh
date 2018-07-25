4ec85e37ccd151e1ad310bdbf6fad84c1f45bb8f
kristof de spiegeleer <kristof@greenitglobe.com>
2018-07-25 05:52:39 +0200

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -12,22 +12,22 @@ function nim_compile {
         if [ -d "$HOME/nim/Nim" ]; then
             cd Nim
             git pull
-            git checkout tags/v0.16.0
+            #git checkout tags/v0.16.0
         else
             git clone https://github.com/nim-lang/Nim.git
             cd Nim
-            #git checkout -b master origin/master
-            git checkout tags/v0.16.0            
+            git checkout -b master origin/master
+            #git checkout tags/v0.16.0            
         fi
         if [ -d "$HOME/nim/Nim/csources" ]; then
             cd csources
             git pull
-            git checkout tags/v0.16.0             
+            #git checkout tags/v0.16.0             
         else
             git clone https://github.com/nim-lang/csources
             cd csources
-            #git checkout -b master origin/master
-            git checkout tags/v0.16.0     
+            git checkout -b master origin/master
+            #git checkout tags/v0.16.0     
         fi
 
         sh build.sh
